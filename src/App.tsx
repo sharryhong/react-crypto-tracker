5088aee293f884fa387e6433853a3c11b8625d78
sharryhong <kshopzoa15@gmail.com>
2022-10-05 22:11:20 +0900

    globalstyle, theme

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,14 @@
 import React from "react";
 import Router from "./Router";
+import GlobalStyle from "./styles/globalStyle";
 
 function App() {
-  return <Router />;
+  return (
+    <>
+      <GlobalStyle />
+      <Router />
+    </>
+  );
 }
 
 export default App;
---
+++ src/styles/theme.ts
@@ -0,0 +1,7 @@
+import { DefaultTheme } from "styled-components";
+
+export const theme: DefaultTheme = {
+  bgColor: "white",
+  textColor: "#222f3e",
+  accentColor: "#5f27cd",
+};
