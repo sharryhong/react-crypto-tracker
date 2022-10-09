36a81a3752853137581c4809942a07ba71fcaf4e
sharryhong <kshopzoa15@gmail.com>
2022-10-09 14:52:08 +0900

    cors 에러 해결

--- src/apis/coin.ts
+++ src/apis/coin.ts
@@ -1,17 +1,15 @@
-const BASE_URL = "https://api.coinpaprika.com/v1";
-
 export async function fetchCoins() {
-  const data = await (await fetch(`${BASE_URL}/coins`)).json();
+  const data = await (await fetch(`/coins`)).json();
   return data.slice(0, 100);
 }
 
 export async function fetchCoinInfo(coinId: string) {
-  const data = await (await fetch(`${BASE_URL}/coins/${coinId}`)).json();
+  const data = await (await fetch(`/coins/${coinId}`)).json();
   return data;
 }
 
 export async function fetchCoinPrice(coinId: string) {
-  const data = await (await fetch(`${BASE_URL}/tickers/${coinId}`)).json();
+  const data = await (await fetch(`/tickers/${coinId}`)).json();
   return data;
 }
 
